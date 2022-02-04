937af6dc1b8559bd57bae01414ed0722913df03b
Egor Tarasov <jorres.tarasov@gmail.com>
2022-02-04 16:15:02 +0300

    fix: inverted binary clauses when learning

--- weekend-experiments/experiments/sample_binary_clauses.py
+++ weekend-experiments/experiments/sample_binary_clauses.py
@@ -18,7 +18,7 @@ from formula_builder import (
 
 from graph import Graph
 
-from pysat.solvers import Maplesat as PysatSolver
+from pysat.solvers import Maplesat as Cadical
 from pycryptosat import Solver
 
 import utils as U
@@ -27,6 +27,7 @@ import domain_preprocessing as DP
 
 
 def extract_info(cnf, pool, g1, g2, metainfo, test_shortname):
+
     incompatible = DP.find_incompatible_nodes(g1, g2)
 
     learnt_clauses = []
@@ -37,6 +38,7 @@ def extract_info(cnf, pool, g1, g2, metainfo, test_shortname):
 
                 for bit_1 in [-1, 1]:
                     for bit_2 in [-1, 1]:
+
                         if (name_1, name_2) in incompatible[bit_1][bit_2]:
                             continue
 
@@ -44,11 +46,13 @@ def extract_info(cnf, pool, g1, g2, metainfo, test_shortname):
                         cnf_var_2 = bit_2 * pool.v_to_id(name_2)
 
                         assumptions = [cnf_var_1, cnf_var_2]
+                        would_be_learned = [-cnf_var_1, -cnf_var_2]
+
                         # t1 = time.time()
                         res = U.solve_with_timeout(solver, assumptions, 1)
                         # t2 = time.time()
                         if not res:
-                            learnt_clauses.append(assumptions)
+                            learnt_clauses.append(would_be_learned)
 
     pysat.formula.CNF(from_clauses=cnf).to_file(
         f"./experiments/sample_binary_clauses/{test_shortname}.cnf"
@@ -74,7 +78,11 @@ def prepare_extract_info(
     shared_cnf, pool = U.prepare_shared_cnf_from_two_graphs(g1, g2)
     shared_cnf = generate_miter_scheme(shared_cnf, pool, g1, g2)
 
-    extract_info(shared_cnf, pool, g1, g2, metainfo, test_shortname)
+    pysat.formula.CNF(from_clauses=shared_cnf).to_file(
+        f"./experiments/sample_binary_clauses/{test_shortname}.cnf"
+    )
+
+    # extract_info(shared_cnf, pool, g1, g2, metainfo, test_shortname)
 
     U.dump_dict(metainfo, metainfo_file)
 
@@ -83,38 +91,52 @@ def compare_using_info(cnf_file, info_filename):
     formula = pysat.formula.CNF(from_file=cnf_file)
     with open(info_filename, "r") as f:
         lines = f.readlines()
-    # for line in lines:
-    #     clause = map(int,line.strip().split(" "))
-    #     formula.append(clause)
 
-    solver = Solver()
-    solver.add_clauses(formula.clauses)
+    for line in lines:
+        clause = list(map(int, line.strip().split(" ")))
+        formula.append(clause)
 
-    t1 = time.time()
-    res, solution = solver.solve()
-    t2 = time.time()
+    formula.to_file("./experiments/sample_binary_clauses/composite.cnf")
 
-    print(f"Solved with info in {t2 - t1} seconds")
+    # solver = Solver()
+    # print("Started solving")
+
+    # t1 = time.time()
+    # solver.add_clauses(formula.clauses)
+    # res, solution = solver.solve()
+    # print(res)
+    # t2 = time.time()
+
+    # print(f"Solved with info in {t2 - t1} seconds")
+
+
+def uninvert(filename, filename_to):
+    with open(filename_to, "w+") as f1:
+        with open(filename, "r") as f2:
+            for line in f2:
+                clause = list(map(int, line.strip().split(" ")))
+                f1.write(f"{-clause[0]} {-clause[1]}\n")
 
 
 def sample_binary_clauses():
-    experiments = ["4_3"]
+    experiments = ["7_4"]
     for test_shortname in experiments:
         left_schema_name = f"BubbleSort_{test_shortname}"
         right_schema_name = f"PancakeSort_{test_shortname}"
 
         left_schema_file = f"./hard-instances/{left_schema_name}.aag"
         right_schema_file = f"./hard-instances/{right_schema_name}.aag"
+        print(left_schema_file, right_schema_file)
 
         # cnf_file = f"./hard-instances/cnf/{test_shortname}.cnf"
         # cnf_naive_file = f"./hard-instances/cnf/{test_shortname}_naive.cnf"
 
-        metainfo_file = f"./experiments/pairs_in_2m_schema/{test_shortname}.txt"
+        # metainfo_file = f"./experiments/pairs_in_2m_schema/{test_shortname}.txt"
         # tasks_dump_file = f"./hard-instances/assumptions/{test_shortname}.txt"
 
-        prepare_extract_info(
-            left_schema_file, right_schema_file, metainfo_file, test_shortname
-        )
+        # prepare_extract_info(
+        #     left_schema_file, right_schema_file, metainfo_file, test_shortname
+        # )
 
         compare_using_info(
             f"./experiments/sample_binary_clauses/{test_shortname}.cnf",
